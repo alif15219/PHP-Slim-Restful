b84b997fb1ecde2b8fa35a4b19a9d50ae4e372fc
alif15219 <59271900+alif15219@users.noreply.github.com>
2020-01-15 16:16:40 +0700

    insert เเละ  select

--- PHP-Slim-Restful/api/index.php
+++ PHP-Slim-Restful/api/index.php
@@ -18,14 +18,17 @@ function register_by_idcard() {
 
 
     try {
-        
-       
+             
         $db = getDB();
-        $strSQL = "SELECT * FROM mem_h_member WHERE (id_card='1900100097227' and (status_id ='01' or status_id ='04') )"; 
+        $strSQL = "SELECT * FROM mem_h_member WHERE (id_card=id_card and (status_id ='01' or status_id ='04') )"; 
         $objParse = oci_parse($db, $strSQL);
         oci_bind_by_name($objParse, ':id_card', $data->id_card);
         oci_execute ($objParse,OCI_DEFAULT);
         $objResult = oci_fetch_array($objParse,OCI_BOTH);
+        
+
+
+
         
        $tmp = "okkkkkkkkkkkkk";
          if($objResult){
@@ -45,32 +48,29 @@ function register_by_idcard() {
    function insert_meet(){
     $request = \Slim\Slim::getInstance()->request();
     $data = json_decode($request->getBody());
-    $datenow = date('Y-m-d');
 
     $MEM_ID=$data->MEM_ID;
     $BR_NO=$data->BR_NO;
-    $GD_DATE= $datenow->BR_NO;
     $MEET_YEAR=$data->MEET_YEAR;
     
     try {
 
         $db = getDB();
         $sql = 'INSERT INTO MEM_MEETING_REGISTER(MEM_ID,BR_NO,GD_DATE,MEET_YEAR) '.
-                'VALUES(:MEM_ID, :BR_NO, :GD_DATE, :MEET_YEAR)';
+                'VALUES(:MEM_ID, :BR_NO, current_timestamp, :MEET_YEAR)';
 
         $compiled = oci_parse($db, $sql);
         oci_bind_by_name($compiled, ':MEM_ID', $MEM_ID);
         oci_bind_by_name($compiled, ':BR_NO', $BR_NO);
-        oci_bind_by_name($compiled, ':GD_DATE', $GD_DATE);
         oci_bind_by_name($compiled, ':MEET_YEAR', $MEET_YEAR);
         oci_execute($compiled);
 
 
 
-        if($compiled){
+        if($sql){
             // $uData=internaladdfoll();	
-            $compiled = json_encode($compiled);
-            echo '{"Data": ' .$compiled . '}';	
+            $sql = json_encode($sql);
+            echo '{"Data": ' .$sql . '}';	
         }else{
             echo"{'status':'error11111'}";
         }
